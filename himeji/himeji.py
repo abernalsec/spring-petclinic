c6311dd04549292ad2e24c23890775afa8f97fc1
Alejandro Bernal <abernal.neo@gmail.com>
2023-02-03 13:31:54 -0500

    Output file for Himeji

--- himeji/himeji.py
+++ himeji/himeji.py
@@ -1,3 +1,5 @@
 import subprocess
 repo_url = "https://github.com/abernalsec/neocloudsploit"
-subprocess.run(["docker", "run", "--rm", "--name", "trivy-sca-vuln", "-v", "/var/run/docker.sock:/var/run/docker.sock", "aquasec/trivy", "repository", repo_url, "-s", "UNKNOWN,MEDIUM,HIGH,CRITICAL", "--scanners", "vuln", "-f", "json", ">", "trivy-sca-vuln.json"])
+
+with open("trivy-sca-vuln.json", "w") as output:
+    subprocess.run(["docker", "run", "--rm", "--name", "trivy-sca-vuln", "-v", "/var/run/docker.sock:/var/run/docker.sock", "aquasec/trivy", "repository", repo_url, "-s", "UNKNOWN,MEDIUM,HIGH,CRITICAL", "--scanners", "vuln", "-f", "json"], stdout=output)
