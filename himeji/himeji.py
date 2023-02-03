ad41bac4699312389d0e8d491a92e518e74bb967
Alejandro Bernal <abernal.neo@gmail.com>
2023-02-03 14:39:58 -0500

    Tuning trivy-sca-vuln

--- himeji/himeji.py
+++ himeji/himeji.py
@@ -2,4 +2,4 @@ import subprocess
 repo_url = "https://github.com/abernalsec/neocloudsploit"
 
 with open("trivy-sca-vuln.json", "w") as output:
-    subprocess.run(["docker", "run", "--rm", "--name", "trivy-sca-vuln", "-v", "/var/run/docker.sock:/var/run/docker.sock", "aquasec/trivy", "repository", repo_url, "-s", "UNKNOWN,MEDIUM,HIGH,CRITICAL", "--scanners", "vuln", "-f", "json"], stdout=output)
+    subprocess.run(["docker", "run", "--rm", "--name", "trivy-sca-vuln", "aquasec/trivy", "repository", repo_url, "-s", "UNKNOWN,MEDIUM,HIGH,CRITICAL", "--scanners", "vuln", "-q", "-f", "json"], stdout=output)
